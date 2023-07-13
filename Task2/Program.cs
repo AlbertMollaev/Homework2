3c546a530d5d4f1d204e912d4eda9f93b37d57bb
AlbertM <mollaev702@gmail.com>
2023-07-13 20:25:43 +0300

    Решили все заданные ДЗ

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,5 +1,5 @@
 ﻿Console.WriteLine("Введите число: ");
 int number = Convert.ToInt32(Console.ReadLine());
-if (number > 5 & number < 8)  Console.WriteLine("Сегодня выходной ");
-if (number < 6) Console.WriteLine("Сегодня рабочий день");
+if (number > 5 & number < 8)  Console.WriteLine("Этот день является выходным. Можно не идти на работу)");
+if (number < 6) Console.WriteLine("Сегодня рабочий день. Пора на работу(");
 if (number > 7) Console.WriteLine("В неделе только 7 дней");
--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,7 +1,19 @@
 ﻿Console.WriteLine("Введите число: ");
 int number = Convert.ToInt32(Console.ReadLine());
-if (number < 100) Console.WriteLine("Это не трехзначное число");
-if (number > 99 & number < 100000) {
+if (number < 100) Console.WriteLine("Третьей цифры нету!");
+if (number > 99 & number < 1000) {
+    int result = number % 10;
+    Console.WriteLine($"3 цифра числа {number} является: {result}");
+} 
+if (number > 999 & number < 10000) {
+    int result = number / 10 % 10;
+    Console.WriteLine($"3 цифра числа {number} является: {result}");     
+}
+if (number > 9999 & number < 100000) {
     int result = number / 100 % 10;
+    Console.WriteLine($"3 цифра числа {number} является: {result}");
+}
+if (number > 99999 & number < 1000000) {
+    int result = number / 1000 % 10;
     Console.WriteLine($"3 цифра числа {number} является: {result}");
 }
